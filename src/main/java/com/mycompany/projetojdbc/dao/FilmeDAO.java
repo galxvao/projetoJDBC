9405ca93b740389ee8091a9099e09bd6b906e0db
Davi Galvao <daviialexx2@gmail.com>
2024-11-04 19:27:28 -0300

    Adicionando o método inserir filme na classe FilmeDAO.java

--- src/main/java/com/mycompany/projetojdbc/dao/FilmeDAO.java
+++ src/main/java/com/mycompany/projetojdbc/dao/FilmeDAO.java
@@ -4,10 +4,35 @@
  */
 package com.mycompany.projetojdbc.dao;
 
+import com.mycompany.projetojdbc.ConexaoDB;
+import com.mycompany.projetojdbc.entities.Filme;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author davi_galvao
  */
 public class FilmeDAO {
+    public void inserirFilme(Filme filme) throws SQLException {
+    String sql = "INSERT INTO Filme (nome) VALUES (?)";
+    
+    try (Connection conn = ConexaoDB.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+        stmt.setString(1, filme.getTitulo());
+        stmt.executeUpdate();
+
+        // Capturar o ID gerado automaticamente
+        try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+            if (generatedKeys.next()) {
+                filme.setId(generatedKeys.getInt(1)); // Definir o ID no objeto Categoria
+            } else {
+                throw new SQLException("Falha ao inserir filme, nenhum ID gerado.");
+            }
+        }
+    }
+}
     
 }
